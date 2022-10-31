a922794df1f6bd83056eb75b0c1a86d3b5c65688
LJM <wjdals997@naver.com>
2022-10-31 14:46:30 +0900

    aa

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import Login from "../components/Account/Login";
 import { Route, Routes } from "react-router-dom";
 import Register from "../components/Account/Register.js";
 import MainSns from "./MainSns";
-import Write_Job_Special from "./Account/Write_Job_Special";
 import Dm from "./Account/Dm";
 import MyProfile from "./Account/MyProfile";
 // import Profile from "./JobSns/Profile";
+import Job_Dash from "./JobSns/Job_Dash";
 
 function App() {
   return (
@@ -17,6 +17,8 @@ function App() {
         <Route path="/register" element={<Register></Register>}></Route>
         <Route path="/mainsns" element={<MainSns></MainSns>}></Route>
         <Route path='/dm' element= {<Dm></Dm>}></Route>
+        <Route path='/jobdash' element= {<Job_Dash></Job_Dash>}></Route>
+
       </Routes>
     </>
   );
