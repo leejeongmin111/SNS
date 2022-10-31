de5909de97db1fad5a3bf33defd132c82be4388f
기철 <a01041135606@gmail.com>
2022-10-31 14:46:54 +0900

    sdfsd

--- src/components/JobSns/Footer.js
+++ src/components/JobSns/Footer.js
@@ -6,13 +6,21 @@ import write from "../../images/write.png";
 import mypage from "../../images/mypage.png";
 
 function Footer() {
+  const scrollToTop = () => {
+    // 페이지 위로 올라가게하는 함수
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    console.log("TO TOP!");
+  };
   return (
     <div className="footer_navigation">
       <div className="footer_container">
         <div className="menu">
           <img className="icon" src={notice} alt="notice" />
           <img className="icon" src={message} alt="message" />
-          <img className="icon" src={home} alt="home" />
+          <img className="icon" src={home} alt="home" onClick={scrollToTop} />
           <img className="icon" src={write} alt="write" />
           <img className="icon" src={mypage} alt="mypage" />
         </div>
