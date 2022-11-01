6f4ed1244ab69cd72266807babc3b804978a5cf6
LJM <wjdals997@naver.com>
2022-11-01 20:57:13 +0900

    Jm

--- src/components/MainSns/MainCard.js
+++ src/components/MainSns/MainCard.js
@@ -3,10 +3,25 @@ import Profile from "./MainProfile";
 import { ReactComponent as CardButton } from "../../images/cardButton.svg";
 import CardMenu from "./MainCardMenu";
 import Comment from "./MainComment";
+import {useState} from "react"
+import * as React from 'react';
+import { Form } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+
+
 
 function Card(props) {
   const { storyBorder, image, comments, likedByText, likedByNumber, hours } =
     props;
+  
+  const [hidden,setHidden] = useState("")
+
+  function handleSubmit (e){
+    e.preventDefault();
+  }
+
 
   return (
     <div className="card">
@@ -36,8 +51,33 @@ function Card(props) {
       </div>
       <div className="timePosted">{hours} HOURS AGO</div>
       <div className="addComment">
-        <div className="commentText">Add a comment...</div>
-        <div className="postText">Post</div>
+
+
+
+      <div className="comments">
+        {comments.map((comment) => {
+          return (
+            <Comment
+              key={comment.id}
+              accountName={comment.user}
+              comment={comment.text}
+            />
+          );
+        })}
+      </div>
+
+      <Box
+        visibility = "{hidden}"
+        component="form"
+        onSubmit={handleSubmit}
+        noValidate
+        sx={{ mt: 1, marginTop: 0 }}
+        >
+        {/* 댓글 입력 창 */}
+        <input type="text"  name="name" maxlength="20" size="75" placeholder="Add a commnet..." ></input>
+        <Button type="submit">post</Button>
+        </Box>
+        
       </div>
     </div>
   );
