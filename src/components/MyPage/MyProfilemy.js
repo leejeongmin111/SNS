2cacb7782f74c34a93851c65f2c9fc406bdb30b6
LJM <wjdals997@naver.com>
2022-11-02 12:10:12 +0900

    JM

--- src/components/MyPage/MyProfilemy.js
+++ src/components/MyPage/MyProfilemy.js
@@ -5,6 +5,21 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Modal from '@mui/material/Modal';
+import Myfollow_Click from "./Myfollow_Click"
+
+// 모달 스타일 설정 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function Profilemy(props) {
   const {
@@ -21,6 +36,11 @@ function Profilemy(props) {
   let accountName = username
     ? username
     : users[Math.floor(Math.random() * users.length)].username;
+
+  // 모달 설정 
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <>
@@ -41,10 +61,10 @@ function Profilemy(props) {
                   <span>게시물 &nbsp;&nbsp; 0</span>
                 </Grid>
                 <Grid item xs={3}>
-                  <span>팔로우 &nbsp;&nbsp; 0</span>
+                  <span onClick={handleOpen}>팔로우 &nbsp;&nbsp; 0</span>
                 </Grid>
                 <Grid item xs={3}>
-                  <span>팔로잉 &nbsp;&nbsp; 0</span>
+                  <span onClick={handleOpen}>팔로잉 &nbsp;&nbsp; 0</span>
                 </Grid>
               </Grid>
             </Box>
@@ -53,6 +73,21 @@ function Profilemy(props) {
         )}
         <a href="/">{urlText}</a>
       </div>
+
+
+      {/* 모달 : 팔로우 팔로잉 창 */}
+      <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Myfollow_Click></Myfollow_Click>
+        </Box>
+      </Modal>
+    </div>
     </>
   );
 }
---
+++ src/components/MyPage/Myfollow_Click.js
@@ -0,0 +1,26 @@
+import Profile from "./MyProfile";
+
+function Myfollow_Click(){
+    return(
+    <>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+
+     </>
+    );
+}
+export default Myfollow_Click;
