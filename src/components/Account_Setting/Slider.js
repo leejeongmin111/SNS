8e122969e0130c58de3c74cdc597d65482c11b43
NGW <nkangwoo16@gmail.com>
2022-11-10 09:21:19 +0900

    adfas

--- src/components/Account_Setting/Slider.js
+++ src/components/Account_Setting/Slider.js
@@ -4,9 +4,9 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "../../styles/Account_Setting/Slider.scss";
 
-import dog1 from "../../images/dog1.jpg";
-import dog2 from "../../images/dog2.jpg";
-import dog3 from "../../images/dog3.jpg";
+import person1 from "../../images/login/p1.JPG";
+import person2 from "../../images/login/p2.JPG";
+import person3 from "../../images/login/p3.JPG";
 
 // 이미지 슬라이드
 
@@ -27,13 +27,13 @@ export default class SimpleSlider extends Component {
       <div>
         <Slider {...settings}>
           <div>
-            <img className="image1" src={dog1} alt="image1" />
+            <img className="image1" src={person1} alt="image1" />
           </div>
           <div>
-            <img className="image2" src={dog2} alt="image2" />
+            <img className="image2" src={person2} alt="image2" />
           </div>
           <div>
-            <img className="image3" src={dog3} alt="image3" />
+            <img className="image3" src={person3} alt="image3" />
           </div>
         </Slider>
       </div>
